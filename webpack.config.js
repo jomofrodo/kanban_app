ec4b4b9119018e2af83241023ca83df064f6c02a
John Moore <jmoore@claresco.com>
2016-03-15 17:49:21 -0700

    Initial React version of app

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,71 @@
-const
-path = require('path');
-const
-merge = require('webpack-merge');
-const
-webpack = require('webpack');
-const
-NpmInstallPlugin = require('npm-install-webpack-plugin');
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
-const
-TARGET = process.env.npm_lifecycle_event;
-const
-PATHS = {
-	app : path.join(__dirname, 'app'),
-	build : path.join(__dirname, 'build')
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
 };
 
-const
-common = {
-	entry : {
-		app : PATHS.app
-	},
-	output : {
-		path : PATHS.build,
-		filename : 'bundle.js'
-	},
-	module : {
-		loaders : [ {
-			// Test expects a RegExp! Note the slashes!
-			test : /\.css$/,
-			loaders : [ 'style', 'css' ],
-			// Include accepts either a path or an array of paths.
-			include : PATHS.app
-		} ]
-	}
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  }
 };
 
-// Default configuration
-if (TARGET === 'start' || !TARGET) {
-	module.exports = merge(common, {
-		devtool : 'eval-source-map',
-		devServer : {
-			contentBase : PATHS.build,
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
 
-			historyApiFallback : true,
-			hot : true,
-			inline : true,
-			progress : true,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
 
-			// Display only errors to reduce the amount of output.
-			stats : 'errors-only',
+      // display only errors to reduce the amount of output
+      stats: 'errors-only',
 
-			// Parse host and port from env so this is easy to customize.
-			host : process.env.HOST,
-			port : process.env.PORT
-		},
-		plugins : [ new webpack.HotModuleReplacementPlugin(),
-				new NpmInstallPlugin({
-					save : true
-				// --save
-				}) ]
-	});
+      // parse host and port from env so this is easy
+      // to customize
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save
+      })
+    ]
+  });
 }
 
-if (TARGET === 'build') {
-	module.exports = merge(common, {});
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
 }
